d44e3c027d6fbb4990515905c1a60c5a56f955bb
Ashok <g.ashok003@gmail.com>
2024-04-25 20:10:55 +0530

    React Elements and React Componenets created produced JSX as well

--- App.js
+++ App.js
@@ -1,33 +1,33 @@
-/**
- * Parcel (Beast)
- * HMR - Hot Module Reloading
- * File Watcher Alogorithem - Writen in C++
- * Bundeling Files
- * Minify Files (JS optimization)
- * Cleaning Code (Console.logs)
- * DEV and PRODUCTION Build 
- * Super Fast Build Alogirithem
- * JS code Optimization
- * Image Optimization
- * Cashing While Development
- * Compression Files (Renames varibles)
- * COmpactible with old version of browser support code generation
- * HTTPS on DEV env support
- * Parcel will manges the Ports also when we run multiple projects
- * Consistent hashing Algorithems to cashing things
- * Zero Configuration
- * Transitive Depencies (Not a Parcel thing but it is what happing in our code over the pakages lock files)
- */
+
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const headElement1 = React.createElement("h2", { id: "title1" }, "Welcome To");
-const headElement2 = React.createElement("h1", { id: "title2" }, "React World");
-const divElement = React.createElement("div", { id: "container" }, [
-  headElement1,
-  headElement2,
-]);
+/* Old Way to create React Elements */
+// const headElement1 = React.createElement("h2", { id: "title1", key: "1" }, "Welcome To");
+// const headElement2 = React.createElement("h1", { id: "title2", key: "2"  }, "React World");
+// const divElement = React.createElement("div", { id: "container" }, [
+//   headElement1,
+//   headElement2,
+// ]);
+
+/* New Way to create react elements */
+
+// React Element
+const title3 = <h3 id="title3" key="2">Game Starts ...</h3>
+
+// React Components
+const Title1 = () => <h2 id="title1" key="1">Welcome To</h2>
+const Title2 = () => <h1 id="title2" key="2">React World</h1>
+const App = () => {
+  return (
+    <div id="container">
+      {Title1()}
+      <Title2 />
+      {title3}
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(divElement);
+root.render(<App />);
